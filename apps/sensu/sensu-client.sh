6c348c24cbeffbb26a8246058bed2fed8510fc10
Michael Davis <Michael Davis>
2016-09-14 21:57:01 -0700

    Changed client script to point to github.

--- apps/sensu/sensu-client.sh
+++ apps/sensu/sensu-client.sh
@@ -13,8 +13,8 @@ enabled=1' | sudo tee /etc/yum.repos.d/sensu.repo
 # Install Sensu
 sudo yum install sensu -y
 
-sudo wget -O /etc/sensu/conf.d/rabbitmq.json http://env.cliqrtech.com/sensu/client/rabbitmq.json
-sudo wget -O /etc/sensu/conf.d/client.json http://env.cliqrtech.com/sensu/client/client.json
+sudo wget -O /etc/sensu/conf.d/rabbitmq.json https://raw.githubusercontent.com/datacenter/cloudcenter-content/master/apps/sensu/rabbitmq.json
+sudo wget -O /etc/sensu/conf.d/client.json https://raw.githubusercontent.com/datacenter/cloudcenter-content/master/apps/sensu/client.json
 
 sudo sed -i "s/%SENSU_SERVER_IP%/$SENSU_SERVER_IP/g" /etc/sensu/conf.d/rabbitmq.json
 sudo sed -i "s/%TIER_NAME%/$cliqrNodeId/g" /etc/sensu/conf.d/client.json
