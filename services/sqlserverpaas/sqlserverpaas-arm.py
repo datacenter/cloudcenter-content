6db986beba751a7f97ffa3c96b79230221129fb1
Michael Davis <mdavis6890@gmail.com>
2016-09-21 09:22:15 -0700

    Bunch of changes to sqlserverpaas

--- services/sqlserverpaas/sqlserverpaas-arm.py
+++ services/sqlserverpaas/sqlserverpaas-arm.py
@@ -4,10 +4,12 @@ import sys
 import pdb
 import os.path
 import json
+import random
 #from haikunator import Haikunator
 from azure.common.credentials import ServicePrincipalCredentials
 from azure.mgmt.resource import ResourceManagementClient
 from azure.mgmt.resource.resources.models import DeploymentMode
+from azure.mgmt.network import NetworkManagementClient
 
 def print_log(msg):
     print("CLIQR_EXTERNAL_SERVICE_LOG_MSG_START")
@@ -26,9 +28,11 @@ def print_ext_service_result(msg):
 
 cmd = sys.argv[1]
 
-my_subscription_id = os.environ.get('AZURE_SUBSCRIPTION_ID')   # your Azure Subscription Id
-my_resource_group = os.environ['parentJobName']+os.environ['parentJobId']            # the resource group for deployment
+my_subscription_id = os.environ.get('AZURE_SUBSCRIPTION_ID') # your Azure Subscription Id
+my_resource_group = os.environ['parentJobName']+os.environ['parentJobId'] # the resource group for deployment
+print("Resource Group: {}".format(my_resource_group))
 #my_pub_ssh_key_path = '~/.ssh/id_rsa.pub'   # the path to your rsa public key file
+network_client = NetworkManagementClient(credentials, os.environ['CliqrCloudAccountId'])
 
 #deployer = Deployer(my_subscription_id, my_resource_group, my_pub_ssh_key_path)
 
@@ -50,7 +54,7 @@ if cmd == "start" :
 
 
     print("Beginning the deployment... \n\n")
-    
+
     # Dict that maps keys of CloudCenter's region names to values of Azure's region names.
     # Used below to control where something is deployed
     regionmap = {
@@ -70,13 +74,20 @@ if cmd == "start" :
 
     # with open('parameters.json', 'r') as armparams_file_fd:
     #     parameters = json.load(armparams_file_fd)
+    serverName = "server-"+os.environ['parentJobName'].replace('_', '-')
     parameters = {
         "parameters": {
             "serverAdminPassword": {
-                "value": "yiebrUp2"
+                "value": os.environ['dbSAPassword']
             },
             "serverAdminUsername": {
-                "value": "mdavis"
+                "value": os.environ['cliqrDatabaseRootUserName']
+            },
+            "server_name": {
+                "value": serverName
+            },
+            "databases_name": {
+                "value": os.environ['dbName']
             }
         }
     }
@@ -93,6 +104,23 @@ if cmd == "start" :
         deployment_properties
     )
     deployment_async_operation.wait()
+
+    result = {
+        'hostName': serverName+"database.windows.net",
+        'ipAddress': serverName+"database.windows.net",
+        'environment': {
+            'instanceName': "instanceName",
+            'instanceType': "instanceType",
+            'serviceType': "serviceType",
+            'productType': "productType",
+            'status': "status",
+            'port': "port",
+            'version': "version"
+        }
+    }
+
+    print_ext_service_result(json.dumps(result))
+
 
     #print("Done deploying!!\n\nYou can connect via: `ssh azureSample@{}.westus.cloudapp.azure.com`".format(deployer.dns_label_prefix))
     print("Done deploying!")
