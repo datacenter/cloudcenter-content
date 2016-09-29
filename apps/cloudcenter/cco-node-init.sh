cb023a59ea5d46dc541695b766639fc1432158ca
Michael Davis <mdavis6890@gmail.com>
2016-09-28 22:38:11 -0700

    Properly quoted strings in conditional tests.

--- apps/cloudcenter/cco-node-init.sh
+++ apps/cloudcenter/cco-node-init.sh
@@ -13,28 +13,28 @@ agentSendLogMessage  "Working Directory: $(pwd)"
 
 if [ ! -e $OSMOSIX_PROD_HOME/.cliqrRebootResumeInit ]; # First pass through this script.
 then
-    if [ -n $ccmIP ]; then
+    if [ -n "$ccmIP" ]; then
         agentSendLogMessage  "Found custom parameter ccmIP = ${ccmIP}"
-    elif [ -n $CliqrTier_ccm_IP ]; then
+    elif [ -n "$CliqrTier_ccm_IP" ]; then
         agentSendLogMessage  "Didn't find custom parameter ccmIP, but found tier ccm with IP address = ${CliqrTier_ccm_IP}"
         ccmIP=$CliqrTier_ccm_IP
     else
          agentSendLogMessage  "Didn't find custom parameter ccmIP or tier called ccm, so you'll have to configure that yourself by running /usr/local/osmosix/bin/cco_config_wizard.sh as root."
     fi
 
-    if [ -n $cloud ]; then
+    if [ -n "$cloud" ]; then
         agentSendLogMessage  "Found custom parameter cloud = ${cloud}"
     else
-        agentSendLogMessage  "Didn't find custom parameter cloudType. Exiting as failed."
+        agentSendLogMessage  "Didn't find custom parameter cloud. Exiting as failed."
         exit 1
     fi
 
-    if [ -z $dlUser ]; then
+    if [ -z "$dlUser" ]; then
         agentSendLogMessage  "Didn't find custom parameter dlUser. Exiting as failed."
         exit 1
     fi
 
-    if [ -z $dlPass ]; then
+    if [ -z "$dlPass" ]; then
         agentSendLogMessage  "Didn't find custom parameter dlPass. Exiting as failed."
         exit 1
     fi
@@ -44,14 +44,33 @@ then
 
     cd /tmp
     sudo wget -N https://$dlUser:$dlPass@download.cliqr.com/dev-20160917.3/installer/core_installer.bin
+    if [ $? -ne 0 ]; then
+        agentSendLogMessage  "Failed downloading core_installer.bin"
+    fi
+
     sudo wget -N https://$dlUser:$dlPass@download.cliqr.com/dev-20160917.3/appliance/cco-installer.jar
+    if [ $? -ne 0 ]; then
+        agentSendLogMessage  "Failed downloading cco-installer.jar"
+    fi
+
     sudo wget -N https://$dlUser:$dlPass@download.cliqr.com/dev-20160917.3/appliance/cco-response.xml
+    if [ $? -ne 0 ]; then
+        agentSendLogMessage  "Failed downloading cco-response.xml"
+    fi
 
     sudo chmod +x core_installer.bin
 
     sudo mv /etc/yum.repos.d/cliqr.repo . #Get our repo config out of the way so that docker installs properly.
     sudo ./core_installer.bin $os $cloud $module
+    if [ $? -ne 0 ]; then
+        agentSendLogMessage  "Failed running core_installer.bin"
+    fi
+
     sudo yum install -y java
+    if [ $? -ne 0 ]; then
+        agentSendLogMessage  "Failed yum install -y java"
+    fi
+
     sudo mv cliqr.repo /etc/yum.repos.d/ #Put the cliqr repo back
 
     sudo sed -i -e "s/\"rabbit_host\" value=\"default\"/\"rabbit_host\" value=\"${CliqrTier_amqp_PUBLIC_IP}\"/" \
