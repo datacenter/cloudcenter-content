828d419dec7a25fa5b57ecebd93d1dd162fe0e92
Michael Davis <mdavis6890@gmail.com>
2016-10-03 22:22:35 -0700

    Fixed to only run migration IP change if it's a migration

--- apps/wordpress/mysqlSvcPostStart.sh
+++ apps/wordpress/mysqlSvcPostStart.sh
@@ -10,8 +10,11 @@ echo "Working Directory: $(pwd)"
 
 env
 
-# Use simple DB script to replace old front-end IP with new front-end IP in database
-# TODO: Could just use '-e' on mysql to just execute this directly from command line without needing separate script.
-wget https://raw.githubusercontent.com/datacenter/cloudcenter-content/master/apps/wordpress/wp_migration.sql
-replaceToken wp_migration.sql '%APP_SERVER_IP%' $CliqrTier_haproxy_2_PUBLIC_IP
-mysql -u root -pwelcome2cliqr < wp_migration.sql
+# Only if app was migrated and therefore should already have data. Otherwise skip.
+if [ "${appMigrating}" = "true" ]; then
+    # Use simple DB script to replace old front-end IP with new front-end IP in database
+    # TODO: Could just use '-e' on mysql to just execute this directly from command line without needing separate script.
+    wget https://raw.githubusercontent.com/datacenter/cloudcenter-content/master/apps/wordpress/wp_migration.sql
+    replaceToken wp_migration.sql '%APP_SERVER_IP%' $CliqrTier_haproxy_2_PUBLIC_IP
+    mysql -u root -pwelcome2cliqr < wp_migration.sql
+fi
