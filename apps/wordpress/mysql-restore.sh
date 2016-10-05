38fdfe80519988447f907f80da9c5e9d1dfe7c77
Michael Davis <mdavis6890@gmail.com>
2016-10-04 21:18:57 -0700

    Changed >> to | tee --append

--- apps/wordpress/mysql-restore.sh
+++ apps/wordpress/mysql-restore.sh
@@ -11,19 +11,21 @@ echo "Working Directory: $(pwd)"
 
 env
 
-#Install S3
+
+
+ #Install S3
 sudo wget -N "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip"
 sudo unzip -o awscli-bundle.zip
-sudo ./awscli-bundle/install -b ~/bin/aws
+sudo ./awscli-bundle/install -b /root/bin/aws
 
 #Configure S3
-sudo mkdir -p ~/.aws
-sudo echo "[default]" > ~/.aws/config
-sudo echo "region=us-west-2" >> ~/.aws/config
-sudo echo "output=json" >> ~/.aws/config
-sudo echo "[default]" > ~/.aws/credentials
-sudo echo "aws_access_key_id=$aws_access_key_id" >> ~/.aws/credentials
-sudo echo "aws_secret_access_key=$aws_secret_access_key" >> ~/.aws/credentials
+sudo mkdir -p /root/.aws
+echo "[default]" | sudo tee --append /root/.aws/config
+echo "region=us-west-1" | sudo tee --append /root/.aws/config
+echo "output=json" | sudo tee --append /root/.aws/config
+echo "[default]" | sudo tee --append /root/.aws/credentials
+echo "aws_access_key_id=$aws_access_key_id" | sudo tee --append /root/.aws/credentials
+echo "aws_secret_access_key=$aws_secret_access_key" | sudo tee --append /root/.aws/credentials
 
 #Download and restore old database
 sudo ~/bin/aws s3 cp s3://$s3path/$migrateFromDepId/dbbak.sql dbbak.sql
--- apps/wordpress/wp-restore.sh
+++ apps/wordpress/wp-restore.sh
@@ -6,24 +6,26 @@ exec > >(tee -a /var/tmp/wp-restore_$$.log) 2>&1
 . /usr/local/osmosix/service/utils/cfgutil.sh
 cd ~
 
-echo "Username: $(whoami)" # Should execute as root
+echo "Username: $(whoami)"
 echo "Working Directory: $(pwd)"
 
 env
 
-#Install S3
+
+
+ #Install S3
 sudo wget -N "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip"
 sudo unzip -o awscli-bundle.zip
-sudo ./awscli-bundle/install -b ~/bin/aws
+sudo ./awscli-bundle/install -b /root/bin/aws
 
 #Configure S3
-sudo mkdir -p ~/.aws
-sudo echo "[default]" > ~/.aws/config
-sudo echo "region=us-west-2" >> ~/.aws/config
-sudo echo "output=json" >> ~/.aws/config
-sudo echo "[default]" > ~/.aws/credentials
-sudo echo "aws_access_key_id=$aws_access_key_id" >> ~/.aws/credentials
-sudo echo "aws_secret_access_key=$aws_secret_access_key" >> ~/.aws/credentials
+sudo mkdir -p /root/.aws
+echo "[default]" | sudo tee --append /root/.aws/config
+echo "region=us-west-1" | sudo tee --append /root/.aws/config
+echo "output=json" | sudo tee --append /root/.aws/config
+echo "[default]" | sudo tee --append /root/.aws/credentials
+echo "aws_access_key_id=$aws_access_key_id" | sudo tee --append /root/.aws/credentials
+echo "aws_secret_access_key=$aws_secret_access_key" | sudo tee --append /root/.aws/credentials
 
 #cd /var/www
 cp /var/www/wp-config.php /tmp
