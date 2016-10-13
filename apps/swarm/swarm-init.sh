d2bee80af532bf3917fc421a9502f448a03aa15f
Michael Davis <mdavis6890@gmail.com>
2016-10-13 13:05:41 -0700

    Added swarm init.

---
+++ apps/swarm/swarm-init.sh
@@ -0,0 +1,38 @@
+#!/bin/bash -x
+exec > >(tee -a /var/tmp/swarm-init_$$.log) 2>&1
+
+. /usr/local/osmosix/etc/.osmosix.sh
+. /usr/local/osmosix/etc/userenv
+. /usr/local/osmosix/service/utils/cfgutil.sh
+. /usr/local/osmosix/service/utils/agent_util.sh
+cd ~
+
+env
+
+echo "Username: $(whoami)" # Should execute as cliqruser
+echo "Working Directory: $(pwd)"
+
+if [ -n "$gitTag" ]; then
+    agentSendLogMessage  "Found gitTag parameter gitTag = ${gitTag}"
+else
+     agentSendLogMessage  "Didn't find custom parameter gitTag. Using gitTag=master"
+     gitTag="swarm"
+fi
+
+
+sudo yum update
+
+sudo tee /etc/yum.repos.d/docker.repo <<-'EOF'
+[dockerrepo]
+name=Docker Repository
+baseurl=https://yum.dockerproject.org/repo/main/centos/7/
+enabled=1
+gpgcheck=1
+gpgkey=https://yum.dockerproject.org/gpg
+EOF
+
+sudo yum install docker-engine
+
+sudo systemctl enable docker.service
+
+sudo systemctl start docker
