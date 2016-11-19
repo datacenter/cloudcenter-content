30c55e33dac38640b2fb021ae89ef748a16c918b
Michael Davis <mdavis6890@gmail.com>
2016-11-19 14:00:02 -0800

    run rabbit_config.sh

--- services/netscalerext/netscaler.py
+++ services/netscalerext/netscaler.py
@@ -1,29 +1,6 @@
 #!/usr/bin/env python
 
-import json
-import httplib2
-import urllib
+from nsnitro import *
 
-#set the headers and the base URL
-headers = {
-    'Content-type': 'application/x-www-form-urlencoded'
-}
-
-url = 'http://http://35.160.83.0//nitro/v1/config/'
-
-#contruct the payload with URL encoding
-payload = {
-    "object":{
-        "login":{
-            "username":"user",
-            "password":"secret"
-        }
-    }
-}
-payload_encoded = urllib.urlencode(payload)
-#create a HTTP object, and use it to submit a POST request
-http = httplib2.Http()
-response, content = http.request(url, 'POST', body=payload_encoded, headers=headers)
-#for debug purposes, print out the headers and the content of the response
-print json.dumps(response, sort_keys=False, indent=4)
-print json.dumps(content, sort_keys=False, indent=4)
+nitro = NSNitro('35.160.83.0', 'nsroot', 'i-02c4a3189f9a04de0')
+nitro.login()
