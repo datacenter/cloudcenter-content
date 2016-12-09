e8939b7d282919bffb3fb59a2fba5b12d4f6fa9b
Michael Davis <mdavis6890@gmail.com>
2016-12-08 21:45:01 -0800

    Added a bunch more various logging, info and error handling.

--- services/cloudformation/createwp.py
+++ services/cloudformation/createwp.py
@@ -3,7 +3,7 @@ import sys
 import os
 import time
 import boto3
-import boto.cloudformation
+# import boto.cloudformation
 import json
 
 
@@ -40,8 +40,11 @@ try:
     cft = boto3.client('cloudformation')
     with open('/cf-template.json', 'r') as template_file_fd:
         template = template_file_fd.read()
-    with open('/cf-params.json', 'r') as params_file_fd:
-        params = json.load(params_file_fd)
+
+    params = None
+    if os.path.isfile('/cf-params.json'):
+        with open('/cf-params.json', 'r') as params_file_fd:
+            params = json.load(params_file_fd)
 
     # TODO: Security review required for IAM capabilties
     create_cft = cft.create_stack(StackName=JOB_NAME, TemplateBody=template, Parameters=params, Capabilities=['CAPABILITY_NAMED_IAM'])
@@ -50,23 +53,59 @@ try:
 
     #Get WebURL for the deployed stack
     #
-    time.sleep(60)
-    conn = boto.cloudformation.connect_to_region(REGION_NAME)
-    get_stack = conn.describe_stacks(stack_id)
+    #time.sleep(60)
+    #conn = boto.cloudformation.connect_to_region(REGION_NAME)
+    #get_stack = conn.describe_stacks(stack_id)
+    stack = cft.describe_stacks(StackName=stack_id)
+
+    i = 0
+    while stack['Stacks'][0]['StackStatus'] not in ['ROLLBACK_COMPLETE', 'CREATE_COMPLETE']:
+        status = stack['Stacks'][0].get('StackStatus', None)
+        reason = stack['Stacks'][0].get('StackStatusReason', None)
+        message = "{}, Reason: {}".format(status, reason)
+        print(message)
+        time.sleep(5)
+        stack = cft.describe_stacks(StackName=stack_id)
+        i =+ 1
+        if i > 120: # break after 10min, just in case.
+            break
 
 
-    checkstatus = []
+    stack_events = cft.describe_stack_events(StackName=stack_id)
+    for event in reversed(stack_events['StackEvents']):
+        status = event.get('ResourceStatus', None)
+        reason = event.get('ResourceStatusReason', None)
+        timestamp = event.get('Timestamp', None)
+        resource_type = event.get('ResourceType', None)
+        message = "{} {} {}, Reason: {}".format(timestamp, resource_type, status, reason)
+        print_log(message)
 
-    while not checkstatus:
-        time.sleep(10)
-        conn = boto.cloudformation.connect_to_region(REGION_NAME)
-        get_stack = conn.describe_stacks(stack_id)
-        stack = get_stack[0]
-        for output in stack.outputs:
-            checkstatus = '%s=%s (%s)' % (output.key, '', output.value)
-            print_log('%s=%s (%s)' % (output.key, '', output.value))
-            if checkstatus != []:
-                break
+    for output in stack['Stacks'][0]['Outputs']:
+        key = output['OutputKey']
+        value = output['OutputValue']
+        desc = output['Description']
+        output_msg = "{}: {}, {}".format(key, value, desc)
+        print_log(output_msg)
+
+    # After all that, if it doesn't look complete, just delete it. At the end to ensure as much info as
+    # possible about what went wrong is passed to UI.
+    if stack['Stacks'][0]['StackStatus'] != 'CREATE_COMPLETE':
+        print_log("Looks like the stack didn't deploy properly for some reason. Deleting it.")
+        cft.delete_stack(StackName=stack_id)
+        sys.exit(1)
+
+    # checkstatus = []
+    #
+    # while not checkstatus:
+    #     time.sleep(10)
+    #     conn = boto.cloudformation.connect_to_region(REGION_NAME)
+    #     get_stack = conn.describe_stacks(stack_id)
+    #     stack = get_stack[0]
+    #     for output in stack.outputs:
+    #         checkstatus = '%s=%s (%s)' % (output.key, '', output.value)
+    #         print_log('%s=%s (%s)' % (output.key, '', output.value))
+    #         if checkstatus != []:
+    #             break
 
 except Exception as err:
     print_log("Error: {0}".format(err))
