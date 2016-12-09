634299acd2a585d43d34af68638c6a04a6b9b63f
Michael Davis <mdavis6890@gmail.com>
2016-12-08 16:47:46 -0800

    Added simple error handling and reporting to UI.

--- services/cloudformation/createwp.py
+++ services/cloudformation/createwp.py
@@ -25,41 +25,45 @@ def print_ext_service_result(msg):
 
 # cmd = sys.argv[1]
 
+try:
+    JOB_NAME = os.environ['parentJobName']
+    TEMPLATE_URL = os.environ['Templateurl']
+    REGION_NAME = os.environ['region']
 
-JOB_NAME = os.environ['parentJobName']
-TEMPLATE_URL = os.environ['Templateurl']
-REGION_NAME = os.environ['region']
-
-symbol = "~`!@#$%^&*()_+={}[]:>;',</?*+"
-for i in JOB_NAME:
-    if i in symbol:
-           print_log("Job Name must satisfy regular expression pattern: [a-zA-Z][-a-zA-Z0-9]*")
-           sys.exit(errno.EINVAL)          
-
-
-cft = boto3.client('cloudformation')
-with open('/cf-template.json', 'r') as template_file_fd:
-    template = template_file_fd.read()
-create_cft = cft.create_stack(StackName=JOB_NAME, TemplateBody=template)
-
-stack_id = create_cft.get("StackId")
-
-#Get WebURL for the deployed stack
-#
-time.sleep(60)
-conn = boto.cloudformation.connect_to_region(REGION_NAME)
-get_stack = conn.describe_stacks(stack_id)
+    symbol = "~`!@#$%^&*()_+={}[]:>;',</?*+"
+    for i in JOB_NAME:
+        if i in symbol:
+               print_log("Job Name must satisfy regular expression pattern: [a-zA-Z][-a-zA-Z0-9]*")
+               sys.exit(errno.EINVAL)
 
 
-checkstatus = []
+    cft = boto3.client('cloudformation')
+    with open('/cf-template.json', 'r') as template_file_fd:
+        template = template_file_fd.read()
+    create_cft = cft.create_stack(StackName=JOB_NAME, TemplateBody=template)
 
-while not checkstatus: 
-    time.sleep(10)
+    stack_id = create_cft.get("StackId")
+
+    #Get WebURL for the deployed stack
+    #
+    time.sleep(60)
     conn = boto.cloudformation.connect_to_region(REGION_NAME)
     get_stack = conn.describe_stacks(stack_id)
-    stack = get_stack[0]
-    for output in stack.outputs:
-        checkstatus = '%s=%s (%s)' % (output.key, '', output.value)
-        print_log('%s=%s (%s)' % (output.key, '', output.value))
-        if checkstatus != []:
-            break    
+
+
+    checkstatus = []
+
+    while not checkstatus:
+        time.sleep(10)
+        conn = boto.cloudformation.connect_to_region(REGION_NAME)
+        get_stack = conn.describe_stacks(stack_id)
+        stack = get_stack[0]
+        for output in stack.outputs:
+            checkstatus = '%s=%s (%s)' % (output.key, '', output.value)
+            print_log('%s=%s (%s)' % (output.key, '', output.value))
+            if checkstatus != []:
+                break
+
+except Exception as err:
+    print_log("Error: {0}".format(err))
+    sys.exit(1)
