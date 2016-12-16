6c8a972cb0b32addb8934755269c144e0ccb4f24
Michael Davis <mdavis6890@gmail.com>
2016-12-16 12:27:02 -0800

    Misc cleanup.

--- apps/cloudcenter/ccm-config.py
+++ apps/cloudcenter/ccm-config.py
@@ -21,90 +21,61 @@ headers = {
 #Create plan
 url = baseUrl+"/v1/tenants/1/plans"
 data = {
-    "name":"planUnl7",
-    "description":"",
-    "type":"UNLIMITED_PLAN",
-    "showOnlyToAdmin":False,
-    "price":"0.00",
-    "onetimeFee":"",
-    "billToVendor":False,
-    "paymentProfileRequired":False,
-    "tenantId":1
+    "name": "planUnl",
+    "description": "",
+    "type": "UNLIMITED_PLAN",
+    "showOnlyToAdmin": False,
+    "price": "0.00",
+    "onetimeFee": "",
+    "billToVendor": False,
+    "paymentProfileRequired": False,
+    "tenantId": 1
+}
+response = s.request("POST", url, data=json.dumps(data), headers=headers, auth=(apiUser, apiPass), verify=False)
+plan = response.json()
+
+# Create contract
+url = baseUrl+"/v1/tenants/1/contracts"
+data = {
+    "name": "Contract",
+    "description": "",
+    "length": "3",
+    "showOnlyToAdmin": False,
+    "terms": "terms",
+    "tenantId": 1,
+    "discountRate": "0"
+}
+response = s.request("POST", url, data=json.dumps(data), headers=headers, auth=(apiUser, apiPass), verify=False)
+contract = response.json()
+
+# Manage plan/contract for user.
+url = baseUrl+"/v1/users/2"
+data = {
+    "action": "MANAGE_PLANS",
+    "userManagePlansData": {
+        "planId": int(plan['id']),
+        "contractId": int(contract['id']),
+        "renewContract": False,
+        "type": "CHANGE_PRORATE",
+        "userId": 2
+    }
 }
 response = s.request("POST", url, data=json.dumps(data), headers=headers, auth=(apiUser, apiPass), verify=False)
 
-# Get CSRF Token
-# url = "https://{ccmIP}/system/service/get_session".format(ccmIP = ccmIP)
+# Agree to Terms
+url = baseUrl+"/acctmgmt/service/agree_term"
+data = {}
+response = s.request("POST", url, data=json.dumps(data), headers=headers, auth=(apiUser, apiPass), verify=False)
 
-# Retrieve the CSRF token first
-# response = s.request("GET", url, verify=False)
-# #soup = BeautifulSoup(response.text, "lxml")
-# #csrftoken = soup.find('input', dict(name='_csrf'))['value']
-#
-# csrftoken = json.loads(response.text)["_csrf"]["token"]
-#
-# # Login
-# url = "https://{ccmIP}/account/login".format(ccmIP = ccmIP)
-#
-# querystring = {}
-# payload = {
-#     "username":"admin@cliqrtech.com,1",
-#     "password":"cliqr",
-#     "request_source" : "",
-#     "_csrf" : csrftoken
-# }
-# headers = {
-#     'origin': "https://54.164.180.248",
-#     'x-devtools-emulate-network-conditions-client-id': "431ae4e3-53f3-4153-9a63-bfd3f8cab801",
-#     'upgrade-insecure-requests': "1",
-#     'user-agent': "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36",
-#     'content-type': "application/x-www-form-urlencoded",
-#     'accept': "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
-#     'referer': url,
-#     'accept-encoding': "gzip, deflate, br",
-#     'accept-language': "en-US,en;q=0.8",
-#     'cache-control': "no-cache",
-#     'postman-token': "0caca4fe-a1d3-03f1-a239-0804c7111986"
-# }
-# response = s.request("POST", url, data=payload, headers=headers, verify=False)
-# lr = response.request
-# print(response.text)
-#
-# # Get API Key
-#
-# url = "https://{ccmIP}/v1/users/2/keys".format(ccmIP = ccmIP)
-#
-# headers = {
-#     'accept': "*/*",
-#     'x-requested-with': "XMLHttpRequest",
-#     'user-agent': "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36",
-#     'referer': "https://54.164.180.248/",
-#     'accept-encoding': "gzip, deflate, sdch, br",
-#     'accept-language': "en-US,en;q=0.8",
-#     'cache-control': "no-cache"
-# }
-#
-# response = s.request("GET", url, headers=headers, verify=False)
-#
-# print(response.text)
-#
-# # Create Plan
-# url = "https://{ccmIP}/v1/tenants/1/plans".format(ccmIP = ccmIP)
-# querystring = {}
-# payload = {
-#     "name":"Test2",
-#     "description":"Initial Unlimited Plan",
-#     "type":"UNLIMITED_PLAN",
-#     "showOnlyToAdmin":False,
-#     "price":"0.00",
-#     "onetimeFee":"",
-#     "billToVendor":False,
-#     "paymentProfileRequired":False,
-#     "tenantId":"1"
-# }
-# headers = {
-#     "Accept":"application/json",
-#     "Content-Type":"application/json"
-# }
-# response = s.request("POST", url, data=json.dumps(payload), headers=headers, params=querystring, verify=False, auth=('admin', 'infoblox'))
-# print(response.text)
+# Add Amazon cloud
+url = baseUrl+"/v1/tenants/1/clouds"
+data = {
+    "name": "Amazon",
+    "description": "",
+    "cloudFamily": "Amazon",
+    "publicCloud": True,
+    "tenantId": 1
+}
+response = s.request("POST", url, data=json.dumps(data), headers=headers, auth=(apiUser, apiPass), verify=False)
+
+
