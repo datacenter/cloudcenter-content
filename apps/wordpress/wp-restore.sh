c228f4724f53fc82148aafcfdd3c0b4d406442d8
fayas-cliqr <fayas@cliqr.com>
2017-01-15 15:47:29 +0530

    Change ~ to /root

--- apps/wordpress/wp-restore.sh
+++ apps/wordpress/wp-restore.sh
@@ -31,7 +31,7 @@ echo "aws_secret_access_key=$aws_secret_access_key" | sudo tee --append /root/.a
 cp /var/www/wp-config.php /tmp
 rm -rf /var/www/*
 
-sudo ~/bin/aws s3 cp s3://$s3path/$migrateFromDepId/wordpressbkup.zip ~/wordpressbkup.zip
+sudo /root/bin/aws s3 cp s3://$s3path/$migrateFromDepId/wordpressbkup.zip ~/wordpressbkup.zip
 sudo unzip -o ~/wordpressbkup.zip -d /var/www
 sudo cp /tmp/wp-config.php /var/www
 sudo chown -R apache:apache /var/www
