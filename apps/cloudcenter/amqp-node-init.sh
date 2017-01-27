4a04e090679c6587dd8c0055ffdf7ccbd27e3d26
Michael Davis <mdavis6890@gmail.com>
2017-01-27 07:13:00 -0800

    Set default tag. A little cleanup.

--- apps/cloudcenter/amqp-node-init.sh
+++ apps/cloudcenter/amqp-node-init.sh
@@ -5,23 +5,22 @@ exec > >(tee -a /var/tmp/amqp-node-init_$$.log) 2>&1
 . /usr/local/osmosix/etc/userenv
 . /usr/local/osmosix/service/utils/cfgutil.sh
 . /usr/local/osmosix/service/utils/agent_util.sh
-#cd ~
-
-#env
 
 echo "Username: $(whoami)" # Should execute as cliqruser
 echo "Working Directory: $(pwd)"
 
+defaultGitTag="cc-full-4.7.0-1"
 if [ -n "$gitTag" ]; then
     agentSendLogMessage  "Found gitTag parameter gitTag = ${gitTag}"
 else
-     agentSendLogMessage  "Didn't find custom parameter gitTag. Using gitTag=master"
-     gitTag="cloudcenter-fullinstall"
+     agentSendLogMessage  "Didn't find custom parameter gitTag. Using gitTag = ${defaultGitTag}"
+     gitTag=${defaultGitTag}
 fi
 
 sudo mv /etc/yum.repos.d/cliqr.repo ~
 sudo yum install -y wget vim java-1.8.0-openjdk nmap
 
+# Download necessary files
 cd /tmp
 wget --no-check-certificate -O core_installer.bin --user $dlUser --password $dlPass https://download.cliqr.com/release-4.7.0-20170105.3/installer/core_installer.bin
 wget --no-check-certificate -O cco-installer.jar --user $dlUser --password $dlPass 	https://download.cliqr.com/release-4.7.0-20170105.3/appliance/cco-installer.jar
