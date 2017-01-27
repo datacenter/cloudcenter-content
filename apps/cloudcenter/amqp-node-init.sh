47df6ed0e377e1c60e0a04eb00596b4ef4e0c314
Michael Davis <mdavis6890@gmail.com>
2017-01-27 08:34:38 -0800

    Added some additional logging.

--- apps/cloudcenter/amqp-node-init.sh
+++ apps/cloudcenter/amqp-node-init.sh
@@ -17,18 +17,25 @@ else
      gitTag=${defaultGitTag}
 fi
 
+agentSendLogMessage  "Installing OS Prerequisits wget vim java-1.8.0-openjdk nmap"
 sudo mv /etc/yum.repos.d/cliqr.repo ~
 sudo yum install -y wget vim java-1.8.0-openjdk nmap
 
 # Download necessary files
 cd /tmp
+agentSendLogMessage  "Downloading installer files."
 wget --no-check-certificate -O core_installer.bin --user $dlUser --password $dlPass https://download.cliqr.com/release-4.7.0-20170105.3/installer/core_installer.bin
 wget --no-check-certificate -O cco-installer.jar --user $dlUser --password $dlPass 	https://download.cliqr.com/release-4.7.0-20170105.3/appliance/cco-installer.jar
 wget --no-check-certificate -O conn_broker-response.xml --user $dlUser --password $dlPass https://download.cliqr.com/release-4.7.0-20170105.3/appliance/conn_broker-response.xml
 
 sudo chmod +x core_installer.bin
+agentSendLogMessage  "Running core installer"
 sudo ./core_installer.bin centos7 amazon rabbit
+
+agentSendLogMessage  "Running jar installer"
 sudo java -jar cco-installer.jar conn_broker-response.xml
+
+agentSendLogMessage  "Running rabbit_config.sh"
 sudo /usr/local/osmosix/bin/rabbit_config.sh
 
 # Use "?" as sed delimiter to avoid escaping all the slashes
