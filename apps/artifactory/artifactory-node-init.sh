67f92e6dd8751de5a7e8401693b81cb48d0e5452
Michael Davis <mdavis6890@gmail.com>
2017-02-20 16:46:11 -0800

    Added download and installation of artifactory files.

--- apps/artifactory/artifactory-node-init.sh
+++ apps/artifactory/artifactory-node-init.sh
@@ -8,11 +8,11 @@ exec > >(tee -a /var/tmp/artifactory-node-init_$$.log) 2>&1
 
 dlFile () {
     agentSendLogMessage  "Attempting to download $1"
-    wget --no-check-certificate $1
+    wget --no-check-certificate $1 -O $2
     if [ "$?" = "0" ]; then
-        agentSendLogMessage  "$1 downloaded"
+        agentSendLogMessage  "${1} downloaded as ${2}"
     else
-        agentSendLogMessage  "Error downloading $1"
+        agentSendLogMessage  "Error downloading ${1}"
         exit 1
     fi
 }
@@ -24,3 +24,8 @@ else
      agentSendLogMessage  "Didn't find custom parameter gitTag. Using gitTag = ${defaultGitTag}"
      gitTag=${defaultGitTag}
 fi
+
+cd /tmp
+dlFile https://bintray.com/jfrog/artifactory-rpms/rpm bintray-jfrog-artifactory-rpms.repo
+sudo mv bintray-jfrog-artifactory-rpms.repo /etc/yum.repos.d/
+sudo yum install jfrog-artifactory-oss
