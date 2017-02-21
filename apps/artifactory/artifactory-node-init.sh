e979b398a0462ca703d9c25a3e2264615192fb8c
Michael Davis <mdavis6890@gmail.com>
2017-02-20 16:36:53 -0800

    Added artifactory.

---
+++ apps/artifactory/artifactory-node-init.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -x
+exec > >(tee -a /var/tmp/artifactory-node-init_$$.log) 2>&1
+
+. /usr/local/osmosix/etc/.osmosix.sh
+. /usr/local/osmosix/etc/userenv
+. /usr/local/osmosix/service/utils/cfgutil.sh
+. /usr/local/osmosix/service/utils/agent_util.sh
+
+dlFile () {
+    agentSendLogMessage  "Attempting to download $1"
+    wget --no-check-certificate $1
+    if [ "$?" = "0" ]; then
+        agentSendLogMessage  "$1 downloaded"
+    else
+        agentSendLogMessage  "Error downloading $1"
+        exit 1
+    fi
+}
+
+defaultGitTag="artifactory"
+if [ -n "$gitTag" ]; then
+    agentSendLogMessage  "Found gitTag parameter gitTag = ${gitTag}"
+else
+     agentSendLogMessage  "Didn't find custom parameter gitTag. Using gitTag = ${defaultGitTag}"
+     gitTag=${defaultGitTag}
+fi
