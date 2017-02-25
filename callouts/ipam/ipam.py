9956d33ba657da3349827cd4b580b68c342783f2
Michael Davis <mdavis6890@gmail.com>
2017-02-24 17:37:47 -0800

    Added stuff to support alternative download locations for the necessary files.

--- callouts/ipam/ipam.py
+++ callouts/ipam/ipam.py
@@ -6,47 +6,41 @@ vpcId = os.getenv("vpcId", None)
 subnetId = os.getenv("subnetId", None)
 networkId = os.getenv("networkId", None)
 networkName = os.getenv("networkName", None)
-nicCount = os.getenv("nicCount")
+nicCount = int(os.getenv("numNICs"))
 
 custSpec = None  # VMware customization spec to use if you want.
 useDhcp = False  # VMware customization spec to use if you want.
 joinDomain = False  # Or true if you want to join a Windows Domain. Then put domain admin creds below.
 
 # OpenStack Specific
-print("portId=")
+print("portId=asdf")
 
 # VMWare Specific
-if custSpec:
-    print("custSpec="+custSpec)
-else:
-    # Windows Specific
-    if joinDomain:
-        print("domainAdminName=")
-        print("domainAdminPassword=")
-    else:
-        print("workgroup=workgroup")
+print("custSpec="+custSpec)
+# Windows Specific
+print("domainAdminName=asdf")
+print("domainAdminPassword=afd")
+print("workgroup=workgroup")
 
-    print("deleteAccounts=false")
-    print("timeZoneId=004")
-    print("organization=CliQr")
-    print("productKey=D2N9P-3P6X9-2R39C-7RTCD-MDVJX")
-    # print("licenseAutoMode=")
-    # print("licenseAutoModeUsers=")
-    print("setAdminPassword=p@ssw0rd")
-    # print("dynamicPropertyName=")
-    # print("dynamicPropertyValue=")
+print("deleteAccounts=false")
+print("timeZoneId=004")
+print("organization=CliQr")
+print("productKey=D2N9P-3P6X9-2R39C-7RTCD-MDVJX")
+# print("licenseAutoMode=")
+# print("licenseAutoModeUsers=")
+print("setAdminPassword=p@ssw0rd")
+# print("dynamicPropertyName=")
+# print("dynamicPropertyValue=")
 
-
-
-print("DnsSuffixList=")
-print("nicDnsServerList=")
-print("domainName=")
-print("hwClockUTC=")
-print("timeZone=")
-print("osHostname=")
+print("DnsSuffixList=cisco.com")
+print("nicDnsServerList=asdf")
+print("domainName=sacliqr.local")
+print("hwClockUTC=true")
+print("timeZone=America/Los_Angeles")
+print("osHostname=asdf")
 
 # Indexed to each NIC
-for i in range(0..nicCount):
+for i in range(0, nicCount):
     print("nicUseDhcp_{}={}".format(i, useDhcp))
     if not useDhcp:
         print("nicIP_{}=192.100.0.81".format(i))
