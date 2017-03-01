6ab960fbe900dc634623dc1aa2a2582dabdb25ce
Michael Davis <mdavis6890@gmail.com>
2017-02-28 16:19:25 -0800

    Added readme.

--- callouts/ipam/ipam.py
+++ callouts/ipam/ipam.py
@@ -8,7 +8,7 @@ networkId = os.getenv("networkId", None)
 networkName = os.getenv("networkName", None)
 nicCount = int(os.getenv("numNICs"))
 
-custSpec = None  # VMware customization spec to use if you want.
+custSpec = "mdavis-workgroup"  # VMware customization spec to use if you want.
 useDhcp = False  # VMware customization spec to use if you want.
 joinDomain = False  # Or true if you want to join a Windows Domain. Then put domain admin creds below.
 
@@ -16,11 +16,15 @@ joinDomain = False  # Or true if you want to join a Windows Domain. Then put dom
 print("portId=asdf")
 
 # VMWare Specific
-print("custSpec="+custSpec)
+if custSpec:
+    print("custSpec="+custSpec)
 # Windows Specific
 print("domainAdminName=asdf")
 print("domainAdminPassword=afd")
 print("workgroup=workgroup")
+
+print("nicCount="+str(nicCount))
+print("fullName=MD")
 
 print("deleteAccounts=false")
 print("timeZoneId=004")
@@ -31,6 +35,9 @@ print("productKey=D2N9P-3P6X9-2R39C-7RTCD-MDVJX")
 print("setAdminPassword=p@ssw0rd")
 # print("dynamicPropertyName=")
 # print("dynamicPropertyValue=")
+print("changeSid=true")
+
+
 
 print("DnsSuffixList=cisco.com")
 print("nicDnsServerList=asdf")
@@ -42,11 +49,11 @@ print("osHostname=asdf")
 # Indexed to each NIC
 for i in range(0, nicCount):
     print("nicUseDhcp_{}={}".format(i, useDhcp))
+    print("nicUseDHCP_{}={}".format(i, useDhcp))
     if not useDhcp:
-        print("nicIP_{}=192.100.0.81".format(i))
+        print("nicIP_{}=192.100.0.90".format(i))
         print("nicNetmask_{}=255.255.255.0".format(i))
         print("DnsServerList_{}=171.70.168.183".format(i))
+        print("nicDnsServerList_{}=171.70.168.183".format(i))
         print("nicGateway_{}=192.100.0.2".format(i))
-
-
 
