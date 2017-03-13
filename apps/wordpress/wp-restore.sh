b81305ab233b59ad44402a3ebd78fc88212ebb39
Michael Davis <mdavis6890@gmail.com>
2017-03-13 07:33:17 -0700

    Changed backup/restore scripts to new wordpress root directory.

--- apps/wordpress/wp-restore.sh
+++ apps/wordpress/wp-restore.sh
@@ -28,13 +28,15 @@ echo "aws_access_key_id=$aws_access_key_id" | sudo tee --append /root/.aws/crede
 echo "aws_secret_access_key=$aws_secret_access_key" | sudo tee --append /root/.aws/credentials
 
 #cd /var/www
-sudo cp /var/www/wp-config.php /tmp
-sudo rm -rf /var/www/*
+sudo cp /var/www/wordpress/wp-config.php /tmp
+sudo rm -rf /var/www/wordpress/*
 
 sudo /root/bin/aws s3 cp s3://$s3path/$migrateFromDepId/wordpressbkup.zip ~/wordpressbkup.zip
-sudo unzip -o ~/wordpressbkup.zip -d /var/www
-sudo cp /tmp/wp-config.php /var/www
-sudo chown -R apache:apache /var/www
+sudo unzip -o ~/wordpressbkup.zip -d /var/www/wordpress
+sudo cp /tmp/wp-config.php /var/www/wordpress
+sudo chown -R apache:apache /var/www/wordpress
 
 sudo rm ~/wordpressbkup.zip
-sudo /root/bin/aws s3 rm --recursive s3://$s3path/$migrateFromDepId
+
+# TODO Fix cleanup for multiple nodes
+# sudo /root/bin/aws s3 rm --recursive s3://$s3path/$migrateFromDepId
