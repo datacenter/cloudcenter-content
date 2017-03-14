52f2f59cc585339948f6ab90761360dba0f9be69
Michael Davis <mdavis6890@gmail.com>
2017-03-14 15:55:01 -0700

    Added logic to create A record.

--- services/route53/route53.py
+++ services/route53/route53.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python
-import sys
 import os
-import time
 import boto3
-# import boto.cloudformation
 import json
 
 
@@ -12,15 +9,26 @@ def print_log(msg):
     print(msg)
     print("CLIQR_EXTERNAL_SERVICE_LOG_MSG_END")
 
+
 def print_error(msg):
     print("CLIQR_EXTERNAL_SERVICE_ERR_MSG_START")
     print(msg)
     print("CLIQR_EXTERNAL_SERVICE_ERR_MSG_END")
 
+
 def print_ext_service_result(msg):
     print("CLIQR_EXTERNAL_SERVICE_RESULT_START")
     print(msg)
     print("CLIQR_EXTERNAL_SERVICE_RESULT_END")
+
+
+def get_hosted_zone_id(domain):
+    response = client.list_hosted_zones()
+    for hosted_zone in response['HostedZones']:
+        if hosted_zone['Name'] in [domain, domain+'.']:
+            return hosted_zone['Id']
+    return False
+
 
 app_domain = os.getenv("appDomain")
 app_hostname = os.getenv("appHostname", None)
@@ -29,10 +37,19 @@ if not app_hostname:
 
 fqdn = "{}.{}".format(app_hostname, app_domain)
 
+# Create list of dependent service tiers
+dependencies = os.environ["CliqrDependencies"].split(",")
+# NOTE: THIS SCRIPT ONLY SUPPORTS THE FIRST DEPENDENT TIER!!!
+
+
+# Set the new server list from the CliQr environment
+serverIps = os.environ["CliqrTier_" + dependencies[0] + "_PUBLIC_IP"].split(",")
+ip_address = serverIps[0]
+
 client = boto3.client('route53')
 
 response = client.change_resource_record_sets(
-    HostedZoneId="/hostedzone/ZX2E8F6AAJENO",
+    HostedZoneId=get_hosted_zone_id(app_domain),
     ChangeBatch={
         'Comment': 'string',
         'Changes': [
@@ -44,7 +61,7 @@ response = client.change_resource_record_sets(
                     'TTL': 1,
                     'ResourceRecords': [
                         {
-                            'Value': '192.0.2.1'
+                            'Value': ip_address
                         },
                     ]
                 }
@@ -52,3 +69,13 @@ response = client.change_resource_record_sets(
         ]
     }
 )
+
+result = {
+    'hostName': fqdn,
+    'ipAddress': ip_address,
+    'environment': {
+        'myEnv': "testEnv"
+    }
+}
+
+print_ext_service_result(json.dumps(result))
