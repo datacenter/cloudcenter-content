85fe290d196e7d2f60d28ba1992f0afab871d5ce
Michael Davis <mdavis6890@gmail.com>
2017-03-14 11:41:55 -0700

    Added new Route 53 service.

---
+++ services/route53/route53.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+. /utils.sh
+
+# Required Service Parameters:
+# gitTag - The tag or branch of code that you want to pull from github
+# TODO
+
+# Print the env to the CCM UI for debugging. Remove this line for production.
+# print_log "$(env)"
+
+defaultGitTag="route53"
+if [ -n "$gitTag" ]; then
+    agentSendLogMessage  "Found gitTag parameter gitTag = ${gitTag}"
+else
+     agentSendLogMessage  "Didn't find custom parameter gitTag. Using gitTag = ${defaultGitTag}"
+     gitTag=${defaultGitTag}
+fi
+
+print_log "Tag/branch for code pull set to $tag"
+
+cmd=$1 # Controls which part of this script is executed based on command line argument. Ex start, stop.
