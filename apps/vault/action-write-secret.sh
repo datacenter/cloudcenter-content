bedf85ce2f6700fb0fb6def325c6da14de87c6ac
Michael Davis <mdavis6890@gmail.com>
2017-06-18 15:37:00 -0700

    Added vault auth.

--- apps/vault/action-write-secret.sh
+++ apps/vault/action-write-secret.sh
@@ -7,4 +7,5 @@ exec > >(tee -a /var/tmp/action-write-secret_$$.log) 2>&1
 . /usr/local/osmosix/service/utils/agent_util.sh
 
 export VAULT_ADDR=http://127.0.0.1:8200
-vault write ${secret_path}
+vault auth ${vault_token}
+vault write ${secret_path} ${vault_secrets}
