2b69d62db54df5bdb4710edb06829b3ddb018a1b
Michael Davis <mdavis6890@gmail.com>
2017-06-19 08:35:23 -0700

    Changed to use wget as curl wasn't working properly with hashicorp server.

--- apps/vault/vault-node-init.sh
+++ apps/vault/vault-node-init.sh
@@ -7,11 +7,14 @@ exec > >(tee -a /var/tmp/vault-node-init_$$.log) 2>&1
 . /usr/local/osmosix/service/utils/agent_util.sh
 
 cd /tmp
-curl -O https://releases.hashicorp.com/vault/0.7.3/vault_0.7.3_linux_amd64.zip
+
+sudo yum install -y wget
+
+wget https://releases.hashicorp.com/vault/0.7.3/vault_0.7.3_linux_amd64.zip
 unzip vault_0.7.3_linux_amd64.zip
 rm vault_0.7.3_linux_amd64.zip
 
-curl -O https://releases.hashicorp.com/consul/0.8.4/consul_0.8.4_linux_amd64.zip
+wget https://releases.hashicorp.com/consul/0.8.4/consul_0.8.4_linux_amd64.zip
 unzip consul_0.8.4_linux_amd64.zip
 rm consul_0.8.4_linux_amd64.zip
 
