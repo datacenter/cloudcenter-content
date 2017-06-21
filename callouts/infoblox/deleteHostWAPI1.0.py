f3c4d604e3557940a29be13cf8a9e773ec9932d9
Michael Davis <mdavis6890@gmail.com>
2017-06-21 16:08:55 -0700

    Fixed up create and delete host scripts.

--- callouts/infoblox/deleteHostWAPI1.0.py
+++ callouts/infoblox/deleteHostWAPI1.0.py
@@ -1,12 +1,29 @@
 #!/usr/bin/env python
 import requests
 import os
-requests.packages.urllib3.disable_warnings()
 
-hostRef = os.getenv('infobloxRef')
+ip_addr = os.getenv('nicIP_0')
 
-#Create Host Record
-url = "https://10.110.5.254/wapi/v1.0/" + hostRef
-payload = ""
+wapi_version = "1.2"
+ib_api_endpoint = "https://10.110.5.254/wapi/v{}".format(wapi_version)
+
+
+s = requests.Session()
+
+url = "{}/ipv4address".format(ib_api_endpoint)
+
+querystring = {"ip_address": ip_addr}
+
 headers = {}
-response = requests.request("DELETE", url, data=payload, headers=headers, verify=False, auth=('admin', 'infoblox'))
+
+response = s.request("GET", url, headers=headers, params=querystring, verify=False,
+                     auth=('admin', 'infoblox'))
+
+print(response.text)
+
+# Delete every object associated to this IP address.
+for obj in response.json()[0]['objects']:
+    url = "{}/{}".format(ib_api_endpoint, obj)
+    s.request("DELETE", url, verify=False,
+              auth=('admin', 'infoblox'))
+
