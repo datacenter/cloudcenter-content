720fcbbc64fa856b69656780bd7c51dde77dca38
Michael Davis <mdavis6890@gmail.com>
2017-06-29 15:40:33 -0700

    Added App Dynamics Controller

--- apps/appd/aic-node-init.sh
+++ apps/appd/aic-node-init.sh
@@ -6,10 +6,10 @@ exec > >(tee -a /var/tmp/aic-node-init_$$.log) 2>&1
 . /usr/local/osmosix/service/utils/cfgutil.sh
 . /usr/local/osmosix/service/utils/agent_util.sh
 
-yum install -y unzip
+# yum install -y unzip
 
-curl -o controller_linux.zip ${appd_installer_url}
+curl -o controller_linux.sh ${appd_installer_url}
 curl -o response.varfile ${appd_response_file}
-unzip controller_linux.zip
-sudo chmod +x controller_64bit_linux-4.3.3.2.sh
-sudo ./controller_64bit_linux-4.3.3.2.sh -q -varfile response.varfile
+#unzip controller_linux.zip
+sudo chmod +x controller_linux.sh
+sudo ./controller_linux.sh -q -varfile response.varfile
