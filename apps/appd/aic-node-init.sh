34719005fa1a6a538ce59505a91eb8880e4c5db8
Michael Davis <mdavis6890@gmail.com>
2017-06-30 07:50:17 -0700

    Set hostname in response file to match the hostname on the VM.

--- apps/appd/aic-node-init.sh
+++ apps/appd/aic-node-init.sh
@@ -12,4 +12,9 @@ curl -o controller_linux.sh ${appd_installer_url}
 curl -o response.varfile ${appd_response_file}
 #unzip controller_linux.zip
 sudo chmod +x controller_linux.sh
+
+echo "serverHostName=${cliqrNodeHostname}" >> response.varfile
+
 sudo ./controller_linux.sh -q -varfile response.varfile
+
+curl -o /home/appduser/AppDynamics/Controller/license.lic ${appd_license}
