1c0face5cec83d9600a6b0e074f0e0ee96533b5e
Michael Davis <mdavis6890@gmail.com>
2017-07-06 13:47:44 -0700

    Adding script for region lifecycle action.

--- apps/appd/appd-region-post-init.sh
+++ apps/appd/appd-region-post-init.sh
@@ -4,21 +4,5 @@
 
 env
 
-appd_controller_ip="172.16.204.34"
-appd_controller_http_port="8090"
-appd_access_key="281c9d49-f465-426f-ab92-ba1c983d434d"
-agentUrl="https://download-files.appdynamics.com/download-file/machine/4.3.3.4/appdynamics-machine-agent-4.3.3.4-1.x86_64.rpm"
-
-cd /tmp
-agentSendLogMessage "Downloading the AppDynamics Machine Agent from ${agentUrl}."
-curl -o appdynamics-machine-agent.rpm ${agentUrl}
-agentSendLogMessage "Installing the AppDynamics Machine Agent."
-sudo rpm -ivh appdynamics-machine-agent.rpm
-agentSendLogMessage "The agent files are installed in opt/appdynamics/machine-agent and the agent is added as a service."
-
-sudo sed -i.bak -e "s%<controller-host>%<controller-host>${appd_controller_ip}%g" \
--e "s%<controller-port>%<controller-port>${appd_controller_http_port}%g" \
--e "s%<account-access-key>%<account-access-key>${appd_access_key}%g" \
-/opt/appdynamics/machine-agent/conf/controller-info.xml
-
-sudo systemctl start appdynamics-machine-agent
+echo "${sshKey}" > key
+chmod 400 key
