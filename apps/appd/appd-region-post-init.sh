e09265d665cb7e3594f56a557ce4efff33951e5b
Michael Davis <mdavis6890@gmail.com>
2017-07-06 14:17:27 -0700

    Remote execution of install script.

--- apps/appd/appd-region-post-init.sh
+++ apps/appd/appd-region-post-init.sh
@@ -4,5 +4,18 @@
 
 env
 
+yum install -y openssh-clients
+
 echo "${sshKey}" > key
 chmod 400 key
+
+tier_ip_varname=CliqrTier_${cliqrAppTierName}_IP
+node_ip=${!tier_ip_varname}
+
+mkdir -p ~/.ssh/
+touch ~/.ssh/known_hosts
+ssh-keyscan ${node_ip} >> ~/.ssh/known_hosts
+
+curl -o https://raw.githubusercontent.com/datacenter/cloudcenter-content/appd/apps/appd/appd-service-centos.sh
+
+ssh -i key cliqruser@${node_ip} 'bash -s' < appd-service-centos.sh
