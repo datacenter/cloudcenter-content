df42b0756abc5a9dd745367090ec7efdb7084b4a
Michael Davis <mdavis6890@gmail.com>
2017-07-06 11:57:45 -0700

    Found the right path for the conf file.

--- apps/appd/appd-service-centos.sh
+++ apps/appd/appd-service-centos.sh
@@ -7,15 +7,21 @@ exec > >(tee -a /var/tmp/appd-service-centos-init_$$.log) 2>&1
 . /usr/local/osmosix/service/utils/cfgutil.sh
 . /usr/local/osmosix/service/utils/agent_util.sh
 
-cd /tmp
+appd_controller_ip="172.16.204.34"
+appd_controller_http_port="8090"
+appd_access_key="281c9d49-f465-426f-ab92-ba1c983d434d"
 agentUrl="https://download-files.appdynamics.com/download-file/machine/4.3.3.4/appdynamics-machine-agent-4.3.3.4-1.x86_64.rpm"
+
+cd /tmp
 agentSendLogMessage "Downloading the AppDynamics Machine Agent from ${agentUrl}."
 curl -o appdynamics-machine-agent.rpm ${agentUrl}
 agentSendLogMessage "Installing the AppDynamics Machine Agent."
 sudo rpm -ivh appdynamics-machine-agent.rpm
 agentSendLogMessage "The agent files are installed in opt/appdynamics/machine-agent and the agent is added as a service."
 
-sudo sed -E "s%<controller-host>%<controller-host>172.16.204.22%g" \
-/etc/appdynamics/machine-agent/controller-info.xml
+sudo sed -i.bak -e "s%<controller-host>%<controller-host>${appd_controller_ip}%g" \
+-e "s%<controller-port>%<controller-port>${appd_controller_http_port}%g" \
+-e "s%<account-access-key>%<account-access-key>${appd_access_key}%g" \
+/opt/appdynamics/machine-agent/conf/controller-info.xml
 
 sudo systemctl start appdynamics-machine-agent
