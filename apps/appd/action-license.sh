f89afd977f566e26e3de65575fac566fd7d151c8
Michael Davis <mdavis6890@gmail.com>
2017-07-10 16:09:41 -0700

    Added custom action to update the license.

--- apps/appd/action-license.sh
+++ apps/appd/action-license.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -x
 exec > >(tee -a /var/tmp/action-license_$$.log) 2>&1
 
-echo ${action_appd_license} > /home/appduser/AppDynamics/Controller/license.lic
+echo "${action_appd_license}" > /home/appduser/AppDynamics/Controller/license.lic
