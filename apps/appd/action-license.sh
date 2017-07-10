6f866e2f4304e51951b65d78b455227d90e134b1
Michael Davis <mdavis6890@gmail.com>
2017-07-10 16:06:03 -0700

    Added custom action to update the license.

--- apps/appd/action-license.sh
+++ apps/appd/action-license.sh
@@ -1,9 +1,4 @@
 #!/bin/bash -x
 exec > >(tee -a /var/tmp/action-license_$$.log) 2>&1
 
-. /usr/local/osmosix/etc/.osmosix.sh
-. /usr/local/osmosix/etc/userenv
-. /usr/local/osmosix/service/utils/cfgutil.sh
-. /usr/local/osmosix/service/utils/agent_util.sh
-
-echo ${appd_license} > /home/appduser/AppDynamics/Controller/license.lic
+echo ${action_appd_license} > /home/appduser/AppDynamics/Controller/license.lic
