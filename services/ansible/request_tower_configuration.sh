983fb1ef42be2da719da2817b13d14c33d34241a
Michael Davis <mdavis6890@gmail.com>
2017-07-14 16:37:36 -0700

    Pulled scripts out of bundle zip.

---
+++ services/ansible/request_tower_configuration.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+usage() {
+  echo -e "Requests server configuration from Ansible Tower\n"
+  echo "Usage: $0 <server address>[:server port] <host config key> <job template id>"
+  echo "Example: $0 example.towerhost.net 44d7507f2ead49af5fca80aa18fd24bc 38"
+  exit 1
+}
+
+[ $# -lt 3 ] && usage
+
+retry_attempts=10
+attempt=0
+while [[ $attempt -lt $retry_attempts ]]
+do
+  status_code=`curl -s -i --data "host_config_key=$2" http://$1/api/v1/job_templates/$3/callback/ | head -n 1 | awk '{print $2}'`
+  if [[ $status_code -ge 300 ]]
+    then
+    echo "${status_code} received, encountered problem, halting."
+    exit 1
+  else
+    exit 0
+  fi
+  attempt=$(( attempt + 1 ))
+  echo "${status_code} received... retrying in 1 minute. (Attempt ${attempt})"
+  sleep 60
+done
+exit 1
