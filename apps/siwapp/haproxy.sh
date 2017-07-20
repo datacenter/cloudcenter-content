4677d04c0f56a17d2b1a5ad8461b3940ec1cca61
Michael Davis <mdavis6890@gmail.com>
2017-07-20 07:51:15 -0700

    Added haproxy node init script.

--- apps/siwapp/haproxy.sh
+++ apps/siwapp/haproxy.sh
@@ -1,1 +1,13 @@
-#!/usr/bin/env bash
+#!/bin/bash -x
+exec > >(tee -a /var/tmp/haproxy-node-init_$$.log) 2>&1
+
+. /usr/local/osmosix/etc/.osmosix.sh
+. /usr/local/osmosix/etc/userenv
+. /usr/local/osmosix/service/utils/cfgutil.sh
+. /usr/local/osmosix/service/utils/agent_util.sh
+
+sudo mv /etc/yum.repos.d/cliqr.repo ~
+
+
+
+sudo mv ~/cliqr.repo /etc/yum.repos.d/
