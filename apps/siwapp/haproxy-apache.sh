ffae4701a725002b26c9c7363476ff9142be6f66
Brandon Beck <beckbran@gmail.com>
2017-07-20 21:10:48 -0500

    Update haproxy-apache.sh

--- apps/siwapp/haproxy-apache.sh
+++ apps/siwapp/haproxy-apache.sh
@@ -31,11 +31,11 @@ backend siwapp_apps
 # Set internal separator to ',' since they're comma-delimited lists.
 temp_ifs=${IFS}
 IFS=','
-ipArr=(${CliqrTier_apache2_PUBLIC_IP}) # Array of IPs in my tier.
+ipArr=(${CliqrTier_siwapp_app_PUBLIC_IP}) # Array of IPs in my tier.
 
 # Iterate through list of hosts to add hosts and corresponding IPs to haproxy config file.
 host_index=0
-for host in $CliqrTier_apache2_HOSTNAME ; do
+for host in $CliqrTier_siwapp_app_HOSTNAME ; do
     sudo su -c "echo 'server ${host} ${ipArr[${host_index}]}:8081 check cookie ${host} inter 5s' >> /etc/haproxy/haproxy.cfg"
     sudo su -c "echo '${ipArr[${host_index}]} ${host}' >> /etc/hosts"
     let host_index=${host_index}+1
