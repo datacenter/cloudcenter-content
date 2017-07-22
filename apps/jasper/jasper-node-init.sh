cf71b77fe43aa317ae307f35c23b7083328d4c64
Michael Davis <mdavis6890@gmail.com>
2017-07-22 15:49:49 -0700

    Started working on Jasper

--- apps/jasper/jasper-node-init.sh
+++ apps/jasper/jasper-node-init.sh
@@ -18,5 +18,4 @@ else
      gitTag=${defaultGitTag}
 fi
 
-
 wget  --no-check-certificate https://downloads.sourceforge.net/project/jasperserver/JasperServer/JasperReports%20Server%20Community%20Edition%206.3.0/jasperreports-server-cp-6.3.0-linux-x64-installer.run?r=http%3A%2F%2Fcommunity.jaspersoft.com%2Fproject%2Fjasperreports-server%2Freleases&ts=1488328500&use_mirror=svwh -O jasperreports-server-cp-6.3.0-linux-x64-installer.run
