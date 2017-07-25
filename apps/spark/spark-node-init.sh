e2519dd4399b33d773937ff212d3cba42923707b
Michael Davis <mdavis6890@gmail.com>
2017-07-25 12:52:33 -0700

    Starting work on Spark app.

--- apps/spark/spark-node-init.sh
+++ apps/spark/spark-node-init.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -x
-exec > >(tee -a /var/tmp/jaspesoft-init_$$.log) 2>&1
+exec > >(tee -a /var/tmp/spark-node-init_$$.log) 2>&1
 
 . /usr/local/osmosix/etc/.osmosix.sh
 . /usr/local/osmosix/etc/userenv
@@ -12,11 +12,20 @@ sudo mv /etc/yum.repos.d/cliqr.repo ~ # Move it back at end of script.
 
 agentSendLogMessage "Doing yum update"
 sudo yum update -y
-#pre_reqs="tomcat postgresql postgresql-server unzip java-1.8.0-openjdk java-1.8.0-openjdk-devel"
-#agentSendLogMessage "Installing pre-reqs: ${pre_reqs}"
-#sudo yum install -y ${pre_reqs}
+pre_reqs="java-1.8.0-openjdk"
+agentSendLogMessage "Installing pre-reqs: ${pre_reqs}"
+sudo yum install -y ${pre_reqs}
 
+cd /tmp
+spark_file="spark.tgz"
+agentSendLogMessage "Downloading ${spark_package}"
+curl -o ${spark_file} "${spark_package}"
+spark_folder=`tar -tzf ${spark_file} | head -n1`
+tar -xvf ${spark_file} -C ~
+rm -f ${spark_file}
 
+ln -s ~/${spark_folder} ~/spark
 
-
+echo "export SPARK_HOME=~/spark" >> ~/.bashrc
+echo 'export PATH=${SPARK_HOME}/bin:${PATH}' >> ~/.bashrc
 sudo mv ~/cliqr.repo /etc/yum.repos.d/
