d84144d593652393186d049a8359d9d6bf57fa75
Michael Davis <mdavis6890@gmail.com>
2017-08-10 07:56:30 -0700

    Trying to get the thing database agent to start as a service. Needs more work.

--- apps/appd/appd-datebase-agent-init.sh
+++ apps/appd/appd-datebase-agent-init.sh
@@ -7,11 +7,6 @@ exec > >(tee -a /var/tmp/appd-database-agent-init_$$.log) 2>&1
 . /usr/local/osmosix/service/utils/cfgutil.sh
 . /usr/local/osmosix/service/utils/agent_util.sh
 
-#appd_controller_ip="172.16.204.34"
-#appd_controller_http_port="8090"
-#appd_access_key="281c9d49-f465-426f-ab92-ba1c983d434d"
-# agentUrl="http://172.16.201.244:8081/artifactory/appd/download-file/php-rpm/4.3.3.4/appdynamics-php-agent.x86_64.rpm"
-
 db_agent_home="/opt/appdynamics/db_agent"
 
 sudo mv /etc/yum.repos.d/cliqr.repo ~ # Move it back at end of script.
@@ -28,29 +23,26 @@ sudo sed -i.bak -e "s%<controller-host>%<controller-host>${appd_controller_ip}%g
 -e "s%<account-access-key>%<account-access-key>${appd_access_key}%g" \
 ${db_agent_home}/conf/controller-info.xml
 
+echo "
+" > start_agent.sh
+sudo mv start_agent.sh
+
+
+echo "
+[Unit]
+Description=AppDDBagentprocess
+Requires=network-online.target
+After=network-online.target
+
+[Service]
+Restart=on-failure
+ExecStart=/usr/bin/java -Ddbagent.name='DB Agent ${parentJobName}' -jar ${db_agent_home}/db-agent.jar
+" > appd-db-agent.service
+
+sudo mv appd-db-agent.service /etc/systemd/system/
+sudo systemctl enable appd-db-agent
+sudo systemctl start appd-db-agent.service
+
 java -Ddbagent.name="DB Agent ${parentJobName}" -jar ${db_agent_home}/db-agent.jar
 
-
 sudo mv ~/cliqr.repo /etc/yum.repos.d/
-
-
-
-
-#
-#agentSendLogMessage "Installing PHP CLI."
-#sudo yum install -y php-cli
-#
-#export APPD_CONF_CONTROLLER_HOST=${appd_controller_ip}
-#export APPD_CONF_CONTROLLER_PORT=${appd_controller_http_port}
-#export APPD_CONF_APP=${parentJobName}
-#export APPD_CONF_TIER=${cliqrNodeHostname}
-#export APPD_CONF_ACCESS_KEY=${appd_access_key}
-#
-#
-#cd /tmp
-#agentSendLogMessage "Downloading the AppDynamics PHP Agent from ${agentUrl}."
-#curl -o appdynamics-php-agent.rpm ${agentUrl}
-#agentSendLogMessage "Installing the AppDynamics PHP Agent."
-#sudo -E rpm -i appdynamics-php-agent.rpm
-#agentSendLogMessage "The agent files are installed in opt/appdynamics/machine-agent and the agent is added as a service."
-#rm -f appdynamics-php-agent.rpm
