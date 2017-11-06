5f7fe284d9ec804fde5b3b57d1c9e447ed895803
Michael Davis <mdavis6890@gmail.com>
2017-11-06 08:18:37 -0800

    Working on service to launch Kube cluster in AWS using kops.

--- services/kubernetes/kops-aws.sh
+++ services/kubernetes/kops-aws.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
+
+# https://kubernetes.io/docs/getting-started-guides/kops/
+
 . /utils.sh
 
-prereqs="unzip wget"
+# Remove this line and add as service custom parameter 'hostedZone'
+hostedZone="cloudcenterdemo.com"
+
+prereqs="unzip wget openssh"
 yum install -y ${prereqs}
 
 #Install AWS CLI
@@ -26,3 +32,23 @@ sudo mv ./kops /usr/local/bin/
 wget -O kubectl https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
 chmod +x ./kubectl
 sudo mv ./kubectl /usr/local/bin/kubectl
+
+ssh-keygen -f /root/.ssh/id_rsa -N
+
+s3bucket="s3://clusters.${hostedZone}"
+aws s3 mb ${s3bucket}
+export KOPS_STATE_STORE=${s3bucket}
+
+clusterName="useast1.${hostedZone}"
+kops create cluster \
+--name=${clusterName} \
+--state=${s3bucket} \
+--zones=us-east-1c \
+--node-count=2
+
+kops update cluster ${clusterName} --yes
+
+kube_config_path="${s3bucket}/${clusterName}/kube_config"
+aws s3 cp /root/.kube/config ${kube_config_path}
+print_log "Kube config file uploaded to ${kube_config_path}"
+
