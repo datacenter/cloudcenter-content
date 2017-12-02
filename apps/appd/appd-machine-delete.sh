be3a3fd425fd1b82383331f6a59f9f70a6de9ae1
Michael Davis <mdavis6890@gmail.com>
2017-12-02 07:54:52 -0800

    Add tag.

--- apps/appd/appd-machine-delete.sh
+++ apps/appd/appd-machine-delete.sh
@@ -4,5 +4,5 @@ yum install -y python-pip
 pip install pip --upgrade
 pip install requests
 
-curl -o appd-machine-delete.py https://raw.githubusercontent.com/datacenter/cloudcenter-content/appd/apps/appd/appd-machine-delete.py
+curl -o appd-machine-delete.py https://raw.githubusercontent.com/datacenter/cloudcenter-content/appd0.1/apps/appd/appd-machine-delete.py
 python appd-machine-delete.py
--- apps/appd/appd-mysql-agent.sh
+++ apps/appd/appd-mysql-agent.sh
@@ -12,7 +12,7 @@ serviceStatus=""
 if [ -n "${appd_db_git_tag}" ]; then
     tag="${appd_db_git_tag}"
 else
-    tag="appd"
+    tag="appd0.1"
 fi
 
 # Setup a bunch of prerequisits
