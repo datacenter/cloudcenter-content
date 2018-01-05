6928b9119c56da77c5c0d31b2e4b0825fccd0283
Brandon Beck <robbeck@cisco.com>
2018-01-04 20:49:45 -0600

    updated galera fixup script

--- apps/siwapp/siwapp-galera-fixup.sh
+++ apps/siwapp/siwapp-galera-fixup.sh
@@ -1,11 +1,6 @@
 #!/bin/bash -x
 
-exec > >(tee -a /var/tmp/apache-node-init_$$.log) 2>&1
-
-. /usr/local/osmosix/etc/.osmosix.sh
 . /usr/local/osmosix/etc/userenv
-. /usr/local/osmosix/service/utils/cfgutil.sh
-. /usr/local/osmosix/service/utils/agent_util.sh
 
 temp_ifs=${IFS}
 IFS=','
@@ -42,6 +37,5 @@ else
         exit 1
     else
         echo "starting slave"
-        systemctl start mariadb
     fi
 fi
