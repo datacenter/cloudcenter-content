c7e01a5fbd1f601b97187cc01951b72dc2a5183e
Michael Davis <mdavis6890@gmail.com>
2018-01-31 12:39:28 -0800

    Moved around some variables to make it more generalized. Added a little more logging.

--- apps/appd/appd-mysql-agent.py
+++ apps/appd/appd-mysql-agent.py
@@ -34,6 +34,9 @@ username = "admin@customer1"
 password = "pa55word"
 tier_name = os.getenv('cliqrAppTierName')
 host_ip = os.getenv("CliqrTier_{}_PUBLIC_IP".format(tier_name))
+db_username = "root"
+db_password = os.getenv('cliqrDatabaseRootPass')
+db_port = 3306
 
 base_url = "http://172.16.204.34:8090/controller/rest"
 
@@ -43,14 +46,10 @@ if cmd == "add":
             "name": coll_name,
             "type": "MYSQL",
             "hostname": host_ip,
-            "username": "root",
-            "password": "welcome2cliqr",
-            "port": 3306,
-            "enableOSMonitor": True,
-            "hostOS": "LINUX",
-            "hostUsername": "root",
-            "hostPassword": "auslab",
-            "sshPort": 22,
+            "username": db_username,
+            "password": db_password,
+            "port": db_port,
+            "enableOSMonitor": False,
             "agentName": "DB Agent a2",
             "enabled": True
         }
@@ -65,7 +64,6 @@ if cmd == "add":
         print_log("Request Payload:\n {}".format(json.dumps(payload, indent=2)))
         # sys.exit(1)
 
-# TODO: Implement MySQL collector delete from AppD
 elif cmd == "remove":
     try:
         headers = {
@@ -73,12 +71,14 @@ elif cmd == "remove":
         }
         r = s.request("GET", url="{url}/databases/collectors/".format(url=base_url), headers=headers,
                       verify=False, auth=HTTPBasicAuth(username, password))
-        print_log(r.status_code)
+        r.raise_for_status()
         all_colls = r.json()
         my_colls = filter(lambda x: x['config']['name'] == coll_name, all_colls)
 
         for coll in my_colls:
             my_id = coll['config']['id']
+            my_name = coll['config']['name']
+            print_log("Deleting collection {} with id {}".format(my_name, my_id))
             headers = {
                 "Content-Type": "application/json"
             }
--- apps/appd/appd-mysql-agent.sh
+++ apps/appd/appd-mysql-agent.sh
@@ -4,9 +4,8 @@
 
 . /utils.sh
 
-print_log "$(env)"
-
 cmd=$1
+print_log "Command: ${cmd}"
 serviceStatus=""
 
 if [ -n "${appd_db_git_tag}" ]; then
@@ -14,14 +13,15 @@ if [ -n "${appd_db_git_tag}" ]; then
 else
     tag="appd1.1"
 fi
-
+print_log "Using appd_git_tag ${tag}"
 # Setup a bunch of prerequisits
 
+print_log "Installing pip and requests"
 pip install --upgrade pip
 pip install --upgrade requests
 
 script_file="appd-mysql-agent.py"
-
-#wget -N $serviceDef -O /serviceDef.json
-curl -o ${script_file} https://raw.githubusercontent.com/datacenter/cloudcenter-content/${tag}/apps/appd/appd-mysql-agent.py
+script_url="https://raw.githubusercontent.com/datacenter/cloudcenter-content/${tag}/apps/appd/appd-mysql-agent.py"
+print_log "Downloading script: ${script_url}"
+curl -o ${script_file} ${script_url}
 python ${script_file} ${cmd}
