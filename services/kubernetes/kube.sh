6df99e6987b44cf260253061173e88ef98fe0668
Michael Davis <mdavis6890@gmail.com>
2018-02-04 16:25:01 -0800

    Changing to use YAML instead of image.

--- services/kubernetes/kube.sh
+++ services/kubernetes/kube.sh
@@ -32,12 +32,15 @@ config="${kube_config}"
 reps="${kube_reps}"
 
 dep_name="dep${parentJobName}"
-namespace="ns${parentJobName}"
+# A valid namespace must consist only of LOWER CASE, digits and hyphens.
+# First use tr to delete everything buy alphanumeric and hyphens
+# then to change upper to lower.
+namespace=$(echo "ns${parentJobName}" | tr -dc '[:alnum:]-' | tr '[:upper:]' '[:lower:]')
 service_name="svc${parentJobName}"
 
 # Copied from https://kubernetes.io/docs/tasks/kubectl/install/
 print_log "Installing kubectl"
-msg=$(curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl) || \
+msg=$(curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl 2>&1) || \
             error "Failed to install kubectl: ${msg}"
 chmod +x kubectl
 mv kubectl /usr/local/bin/
@@ -50,19 +53,20 @@ echo "${config}" > ~/.kube/config
 
 case ${cmd} in
     start)
-        msg=$(kubectl create namespace ${namespace}) || \
+        print_log "Creating namespace ${namespace}."
+        msg=$(kubectl create namespace ${namespace} 2>&1) || \
             error "Failed to create namespace: ${msg}"
         print_log "Namespace ${namespace} created"
 
         print_log "${yaml_url}"
         yaml_file="file.yaml"
-        msg=$(curl --fail -o "${yaml_file}" "${yaml_url}") || \
+        msg=$(curl --fail -o "${yaml_file}" "${yaml_url}" 2>&1) || \
             error "Failed downloading yaml file: ${yaml_url}"
 
         print_log "Removing namespace from source yaml, if exists. Using namespace ${namespace} instead."
         sed -i -e '/namespace:/d' ${yaml_file}
 
-        msg=$(kubectl apply --namespace ${namespace} -f ${yaml_file}) || \
+        msg=$(kubectl apply --namespace ${namespace} -f ${yaml_file} 2>&1) || \
             error "Failed applying yaml file."
 
         print_log "Deployment finished, but it may still take a few minutes for the
@@ -71,7 +75,7 @@ case ${cmd} in
     stop)
         print_log "Deleting namespace ${namespace} and all resources in it."
 
-        msg=$(kubectl delete namespace ${namespace}) || \
+        msg=$(kubectl delete namespace ${namespace} 2>&1) || \
             error "Failed to delete the resources: ${msg}"
 
         print_log "Waiting for namespace to terminate."
