2f2513d45ca7d6986d3ef58d2d0a914eccc7c16c
davgoldm <davgoldm@cisco.com>
2018-06-20 10:30:16 -0700

    added - between streamName and parentJobName for clearer stream name

--- services/kinesisstream/kinesisstream.sh
+++ services/kinesisstream/kinesisstream.sh
@@ -42,7 +42,7 @@ if [ -z "$streamName" ];
 then
 	error "Stream Name missing"
 else
-    streamName="$streamName$parentJobName"
+    streamName="$streamName-$parentJobName"
 fi
 
 if [ -z "$shardCount" ];
