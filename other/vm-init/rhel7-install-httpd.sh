00e196a08f0e658393b55b1c6c77ca7ddecee53f
Jason Grimm <jasgrimm@cisco.com>
2018-09-13 17:41:00 -0400

    added sudo to httpd script

--- other/vm-init/rhel7-install-httpd.sh
+++ other/vm-init/rhel7-install-httpd.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
-yum -y --skip-broken install httpd
+sudo yum -y --skip-broken install httpd
 
-service httpd start
-service httpd status
-systemctl enable httpd
+sudo service httpd start
+sudo service httpd status
+sudo systemctl enable httpd
 
-echo 'ServerName 127.0.0.1' >> /etc/httpd/conf/httpd.conf
+sudo echo 'ServerName 127.0.0.1' >> /etc/httpd/conf/httpd.conf
 
-firewall-cmd --zone=public --add-port=80/tcp --permanent
-firewall-cmd --zone=public --add-port=443/tcp --permanent
-firewall-cmd --reload
+sudo firewall-cmd --zone=public --add-port=80/tcp --permanent
+sudo firewall-cmd --zone=public --add-port=443/tcp --permanent
+sudo firewall-cmd --reload
 
-echo "APACHE on RHEL7" > /var/www/html/index.html
+sudo echo "APACHE on RHEL7" > /var/www/html/index.html
 
-service httpd restart
+sudo service httpd restart
 
 exit 0
